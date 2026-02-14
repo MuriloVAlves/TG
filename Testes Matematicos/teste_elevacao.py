e9a189c9fc976009b9e41085a7343aec8baabe5b
Murilo Alves <murilo.alves.ma2@gmail.com>
2026-02-14 15:05:53 -0300

    Added antenna pole vector

--- Testes Matematicos/teste_elevacao.py
+++ Testes Matematicos/teste_elevacao.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 import math
 
-#Script variables
-STEPS = 3
+#SCRIPT VARIABLE
+STEPS = 30
+HINGESTEPS = 10
+LOWBORDER = 0.1
+HIGHBORDER = 0.95
+LOWSIZE = 0.5
+HIGHSIZE = 3
 
 # Parts of the mechanism
 # fixed hinge/movable hinge
 pole = [(0,1),(0,0)] # x/y coordinates
 antenna_hinge = [(0,1),(0,0)]
 actuator = [(0,0),(1,0)]
+antenna_pole = [(0,1),(0,1.1)]
 
-deg2rad = math.pi/180
+DEG2RAD = math.pi/180
 
 def calcular_tamanho(posicao):
     pos1 = posicao[0]
@@ -20,7 +27,7 @@ def calcular_tamanho(posicao):
     deltaY = abs(pos1[1]-pos2[1])
     return math.sqrt((deltaX**2)+(deltaY**2))
 
-def calcular_angulo(c,a,b):
+def calcular_triangulo(c,a,b):
     sqr_triang = (c**2)-((a**2)+(b**2))
     cos_multiplier = -(2*a*b)
     return math.acos(sqr_triang/cos_multiplier)
@@ -41,20 +48,39 @@ def vector2matplot(membro):
     y_coords.append(membro[1][1])
     return x_coords,y_coords
 
-#SCRIPT
-colors = ['red','blue','green','orange','purple']
-numcolor = 0
-STEPS = len(colors)
-for actuator_len in np.arange(0.1,calcular_tamanho(actuator),calcular_tamanho(actuator)/STEPS):
-    active_actuator = [(0,0),(actuator_len,0)]
-    hinge_angle = calcular_angulo(calcular_tamanho(active_actuator),calcular_tamanho(pole),calcular_tamanho(antenna_hinge))
-    actuator_angle = calcular_angulo(calcular_tamanho(antenna_hinge),calcular_tamanho(pole),calcular_tamanho(active_actuator))
-    new_hinge_pos = calcular_posicao(antenna_hinge,hinge_angle+(270*deg2rad))
-    new_active_act_pos = calcular_posicao(active_actuator,(90*deg2rad)-actuator_angle)
-    new_act_pos = calcular_posicao(actuator,(90*deg2rad)-actuator_angle)
-    
-    for part in [pole,new_hinge_pos,new_act_pos,new_active_act_pos]:
-        x_coords,y_coords = vector2matplot(part)
-        plt.plot(x_coords, y_coords, marker='o',color=colors[numcolor])
-    numcolor += 1
-plt.show()
+def main():
+    #SCRIPT
+    css_colors_dict = mcolors.CSS4_COLORS
+    css_color_names = list(css_colors_dict.keys())
+    colors = css_color_names #['red','blue','green','orange','purple','yellow']
+    for multiplier in np.arange(LOWSIZE,HIGHSIZE+((HIGHSIZE-LOWSIZE)/HINGESTEPS),(HIGHSIZE-LOWSIZE)/HINGESTEPS):
+        multiplied_tuple = tuple(item * multiplier for item in pole[0])
+        pole[0] = multiplied_tuple
+        antenna_hinge[0] = multiplied_tuple
+        antenna_pole[0] = pole[0]
+        result_array = np.add(pole[0], (0,0.1)) 
+        result_tuple = tuple(result_array)
+        antenna_pole[1] = result_tuple
+        numcolor = 0
+        print('pole =',calcular_tamanho(pole))
+        print('antenna_hinge =',calcular_tamanho(antenna_hinge))
+        print('antenna_pole =',calcular_tamanho(antenna_pole))
+        for actuator_len in np.arange(calcular_tamanho(actuator)*LOWBORDER,calcular_tamanho(actuator)*HIGHBORDER+calcular_tamanho(actuator)/STEPS,calcular_tamanho(actuator)/STEPS):
+            active_actuator = [(0,0),(actuator_len,0)]
+            try:
+                hinge_angle = calcular_triangulo(calcular_tamanho(active_actuator),calcular_tamanho(pole),calcular_tamanho(antenna_hinge))
+                actuator_angle = calcular_triangulo(calcular_tamanho(antenna_hinge),calcular_tamanho(pole),calcular_tamanho(active_actuator))
+            except:
+                continue
+            new_hinge_pos = calcular_posicao(antenna_hinge,hinge_angle+(270*DEG2RAD))
+            new_active_act_pos = calcular_posicao(active_actuator,(90*DEG2RAD)-actuator_angle)
+            new_act_pos = calcular_posicao(actuator,(90*DEG2RAD)-actuator_angle)
+            new_antenna_pole = calcular_posicao(antenna_pole,(90*DEG2RAD)+hinge_angle)
+            for part in [pole,new_hinge_pos,new_act_pos,new_active_act_pos,new_antenna_pole]:
+                x_coords,y_coords = vector2matplot(part)
+                plt.plot(x_coords, y_coords, marker='o',color=colors[numcolor%len(colors)])
+            numcolor += 1
+        plt.show()
+
+if __name__ == "__main__":
+    main()
