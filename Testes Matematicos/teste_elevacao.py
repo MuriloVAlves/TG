84c6423f4cc717a7bc22b63fc225e797b2555156
Murilo Alves <murilo.alves.ma2@gmail.com>
2026-04-20 16:26:48 -0300

    Fixed elevation algorithm (Needs to fix added angle)

--- Testes Matematicos/teste_elevacao.py
+++ Testes Matematicos/teste_elevacao.py
@@ -9,7 +9,7 @@ HINGESTEPS = 100
 LOWBORDER = 0.1
 HIGHBORDER = 0.95
 LOWSIZE = 0.01
-HIGHSIZE = 1.1
+HIGHSIZE = 3
 MAX_ANTENNA_ANGLE = 90
 MIN_ANTENNA_ANGLE = 0
 ANTENNA_ANGLE_TOLERANCE = 0.2
@@ -65,73 +65,96 @@ def main():
     colors = css_color_names #['red','blue','green','orange','purple','yellow']
     antenna_params = []
     sensitivity = []
-    for multiplier in np.arange(LOWSIZE,HIGHSIZE+((HIGHSIZE-LOWSIZE)/HINGESTEPS),(HIGHSIZE-LOWSIZE)/HINGESTEPS):
-        print('Calculando para',multiplier)
-        pole = ref_pole.copy()
-        antenna_hinge = ref_antenna_hinge.copy()
-        antenna_pole = ref_antenna_pole.copy()
-        multiplied_tuple = tuple(item * multiplier for item in ref_pole[0])
-        pole[0] = multiplied_tuple
-        antenna_hinge[0] = multiplied_tuple
-        antenna_pole[0] = pole[0]
-        result_array = np.add(pole[0], (0,0.1)) 
-        result_tuple = tuple(result_array)
-        antenna_pole[1] = result_tuple
-        numcolor = 0
-        max_angle_value_read = -360
-        max_angle_depth = 0
-        max_angle_found = False
-        min_angle_value_read = 360
-        min_angle_depth = 0
-        min_angle_found = False
-        # print('pole =',calcular_tamanho(pole))
-        # print('antenna_hinge =',calcular_tamanho(antenna_hinge))
-        # print('antenna_pole =',calcular_tamanho(antenna_pole))
-        for actuator_len in np.arange(calcular_tamanho(ref_actuator)*LOWBORDER,(calcular_tamanho(ref_actuator)*HIGHBORDER),calcular_tamanho(ref_actuator)/STEPS):
-            active_actuator = [(0,0),(actuator_len,0)]
-            try:
-                hinge_angle = calcular_triangulo(calcular_tamanho(active_actuator),calcular_tamanho(pole),calcular_tamanho(antenna_hinge))
-                actuator_angle = calcular_triangulo(calcular_tamanho(antenna_hinge),calcular_tamanho(pole),calcular_tamanho(active_actuator))
-            except:
-                continue
-            new_hinge_pos = calcular_posicao(antenna_hinge,hinge_angle+(270*DEG2RAD))
-            new_active_act_pos = calcular_posicao(active_actuator,(90*DEG2RAD)-actuator_angle)
-            new_act_pos = calcular_posicao(ref_actuator,(90*DEG2RAD)-actuator_angle)
-            new_antenna_pole = calcular_posicao(antenna_pole,(90*DEG2RAD)+hinge_angle)
-            # Calcular sensibilidade do sistema
-            # Ângulo atual da antena
-            actual_angle = calcular_angulo(new_antenna_pole)/DEG2RAD
-            # Ver se ângulo maior é maior que o ângulo esperado
-            if actual_angle > max_angle_value_read:
-                #Salvar parâmetros
-                max_angle_found = True
-                max_angle_depth = actuator_len
-                max_angle_value_read = actual_angle
-            #Ver se ele é menor que o menor ângulo maior que o esperado
-            if actual_angle < min_angle_value_read:
-                min_angle_found = True
-                min_angle_depth = actuator_len
-                min_angle_value_read = actual_angle
-            # for part in [pole,new_hinge_pos,new_act_pos,new_active_act_pos,new_antenna_pole]:
-            #     x_coords,y_coords = vector2matplot(part)
-            #     plt.plot(x_coords, y_coords, marker='o',color=colors[numcolor%len(colors)])
-            # numcolor += 1
-        if max_angle_found and min_angle_found and (max_angle_value_read-min_angle_value_read != 0):
-            antenna_params.append((pole,antenna_hinge,antenna_pole))
-            sensitivity.append(((min_angle_depth,max_angle_depth),(min_angle_value_read,max_angle_value_read)))
-        # plt.show()
+    for hingeMultip in np.arange(LOWSIZE,HIGHSIZE+((HIGHSIZE-LOWSIZE)/HINGESTEPS),(HIGHSIZE-LOWSIZE)/HINGESTEPS):
+        for multiplier in np.arange(LOWSIZE,HIGHSIZE+((HIGHSIZE-LOWSIZE)/HINGESTEPS),(HIGHSIZE-LOWSIZE)/HINGESTEPS):
+            print(f'Calculando para pole {multiplier:.4f} e hinge {hingeMultip:.4f}',end='                                       \r')
+            pole = ref_pole.copy()
+            antenna_hinge = ref_antenna_hinge.copy()
+            antenna_pole = ref_antenna_pole.copy()
+            multiplied_pole = tuple(item * multiplier for item in ref_pole[0])
+            multiplied_hinge = tuple(item * hingeMultip for item in ref_antenna_hinge[0])
+            pole[0] = multiplied_pole
+            antenna_hinge[0] = multiplied_hinge
+            antenna_pole[0] = pole[0]
+            result_array = np.add(pole[0], (0,0.1)) 
+            result_tuple = tuple(result_array)
+            antenna_pole[1] = result_tuple
+            numcolor = 0
+            max_angle_value_read = -360
+            max_angle_depth = 0
+            max_angle_found = False
+            min_angle_value_read = 360
+            min_angle_depth = 0
+            min_angle_found = False
+            # print('pole =',calcular_tamanho(pole))
+            # print('antenna_hinge =',calcular_tamanho(antenna_hinge))
+            # print('antenna_pole =',calcular_tamanho(antenna_pole))
+            for actuator_len in np.arange(calcular_tamanho(ref_actuator)*LOWBORDER,(calcular_tamanho(ref_actuator)*HIGHBORDER),calcular_tamanho(ref_actuator)/STEPS):
+                active_actuator = [(0,0),(actuator_len,0)]
+                try:
+                    hinge_angle = calcular_triangulo(calcular_tamanho(active_actuator),calcular_tamanho(pole),calcular_tamanho(antenna_hinge))
+                    actuator_angle = calcular_triangulo(calcular_tamanho(antenna_hinge),calcular_tamanho(pole),calcular_tamanho(active_actuator))
+                except:
+                    continue
+                new_hinge_pos = calcular_posicao(antenna_hinge,hinge_angle+(270*DEG2RAD))
+                new_active_act_pos = calcular_posicao(active_actuator,(90*DEG2RAD)-actuator_angle)
+                new_act_pos = calcular_posicao(ref_actuator,(90*DEG2RAD)-actuator_angle)
+                new_antenna_pole = calcular_posicao(antenna_pole,(90*DEG2RAD)+hinge_angle)
+                # Calcular sensibilidade do sistema
+                # Ângulo atual da antena
+                actual_angle = calcular_angulo(new_antenna_pole)/DEG2RAD
+                # Ver se ângulo maior é maior que o ângulo esperado
+                if actual_angle > max_angle_value_read:
+                    #Salvar parâmetros
+                    max_angle_found = True
+                    max_angle_depth = actuator_len
+                    max_angle_value_read = actual_angle
+                #Ver se ele é menor que o menor ângulo maior que o esperado
+                if actual_angle < min_angle_value_read:
+                    min_angle_found = True
+                    min_angle_depth = actuator_len
+                    min_angle_value_read = actual_angle
+                # for part in [pole,new_hinge_pos,new_act_pos,new_active_act_pos,new_antenna_pole]:
+                #     x_coords,y_coords = vector2matplot(part)
+                #     plt.plot(x_coords, y_coords, marker='o',color=colors[numcolor%len(colors)])
+                # numcolor += 1
+            if max_angle_found and min_angle_found and (max_angle_value_read-min_angle_value_read != 0):
+                antenna_params.append((pole,antenna_hinge,antenna_pole))
+                sensitivity.append(((min_angle_depth,max_angle_depth),(min_angle_value_read,max_angle_value_read)))
+            # plt.show()
     # Verificar a melhor sensibilidade
-    print('Verificando os melhores parâmetros encontrados...')
+    print('\nVerificando os melhores parâmetros encontrados...')
     best_guess_value = (-360,360)
     best_guess_actuator = (0,0)
     best_guess_index = -1
-    for i in range(len(sensitivity)):
-        actuator,value = sensitivity[i]
-        if actuator[0] - actuator[1] >= best_guess_actuator[0] - best_guess_actuator[1]:
-            if value[1] - value[0] >= ANTENNA_ANGLE_TOLERANCE+MAX_ANTENNA_ANGLE-MIN_ANTENNA_ANGLE:
-                if value[1] - value[0] <= best_guess_value[1] - best_guess_value[0]:
+    latch = True
+    reset_params = True
+    iteracao = 1
+    while latch:
+        print(f'Iteração {iteracao}',end='\r')
+        iteracao += 1
+        if reset_params:
+            reset_params = False
+            best_guess_value = (-360,360)
+        else:
+            latch = False
+        for i in range(len(sensitivity)):
+            actuator,value = sensitivity[i]
+            # Checks if has higher range
+            if abs(actuator[0] - actuator[1]) > abs(best_guess_actuator[0] - best_guess_actuator[1]):
+                reset_params = True
+                best_guess_actuator = actuator
+            # Checks if covers rotation
+            elif (abs(value[1] - value[0]) >= ANTENNA_ANGLE_TOLERANCE+MAX_ANTENNA_ANGLE-MIN_ANTENNA_ANGLE) and (abs(actuator[0] - actuator[1]) == abs(best_guess_actuator[0] - best_guess_actuator[1])):
+                #Check if has lower overhead
+                if abs(value[1] - value[0]) <= abs(best_guess_value[1] - best_guess_value[0]):
+                    print('\nMelhor parâmetro encontrado!')
+                    print('Range:',abs(actuator[0] - actuator[1]))
+                    print('Delta:',abs(value[1] - value[0]))
+                    print(best_guess_actuator, "<=", actuator)
+                    print(best_guess_value, "<=", value)
+                    print(best_guess_index, "<=", i)
                     best_guess_value = value
-                    best_guess_actuator = actuator
                     best_guess_index = i
 
     # Corrigir braço da antena
@@ -146,55 +169,56 @@ def main():
             if abs(min_val-MIN_ANTENNA_ANGLE)+abs(max_val-MAX_ANTENNA_ANGLE) < best_guess_difference:
                 best_guess_correction = correc_angle
                 best_guess_difference = abs(min_val-MIN_ANTENNA_ANGLE)+abs(max_val-MAX_ANTENNA_ANGLE)
+                best_guess_angles = (min_val,max_val)
     # Printar parâmetros:
-    print('\r--- Melhores parâmetros ---')
+    print('--- Melhores parâmetros ---')
     best_guess_params = antenna_params[best_guess_index]
-    print('pole =',calcular_tamanho(best_guess_params[0]))
-    print('antenna_hinge =',calcular_tamanho(best_guess_params[1]))
-    print('antenna_pole =',calcular_tamanho(best_guess_params[2]))
-    print('antenna_correction = ',best_guess_correction)
-    print('actuator_range = ',best_guess_actuator)
+    print(f'pole = {calcular_tamanho(best_guess_params[0]):.4f}')
+    print(f'antenna_hinge = {calcular_tamanho(best_guess_params[1]):.4f}')
+    print(f'antenna_pole = {calcular_tamanho(best_guess_params[2]):.4f}')
+    print(f'antenna_correction = {best_guess_correction:.4f}')
+    print(f'actuator_range = {best_guess_actuator}')
+    print(f'antenna_range = {best_guess_angles}')
 
     # Resultados gráfico:
-    # pole = best_guess_params[0]
-    # antenna_hinge = best_guess_params[1]
-    # antenna_pole = pole[0]
-    # result_array = np.add(pole[0], (0,0.1)) 
-    # result_tuple = tuple(result_array)
-    # antenna_pole[1] = result_tuple
-    # numcolor = 0
-    # for actuator_len in np.arange(calcular_tamanho(ref_actuator)*LOWBORDER,(calcular_tamanho(ref_actuator)*HIGHBORDER),calcular_tamanho(ref_actuator)/STEPS):
-    #     active_actuator = [(0,0),(actuator_len,0)]
-    #     try:
-    #         hinge_angle = calcular_triangulo(calcular_tamanho(active_actuator),calcular_tamanho(pole),calcular_tamanho(antenna_hinge))
-    #         actuator_angle = calcular_triangulo(calcular_tamanho(antenna_hinge),calcular_tamanho(pole),calcular_tamanho(active_actuator))
-    #     except:
-    #         continue
-    #     new_hinge_pos = calcular_posicao(antenna_hinge,hinge_angle+(270*DEG2RAD))
-    #     new_active_act_pos = calcular_posicao(active_actuator,(90*DEG2RAD)-actuator_angle)
-    #     new_act_pos = calcular_posicao(ref_actuator,(90*DEG2RAD)-actuator_angle)
-    #     new_antenna_pole = calcular_posicao(antenna_pole,(90*DEG2RAD)+hinge_angle)
-    #     # Calcular sensibilidade do sistema
-    #     # Ângulo atual da antena
-    #     actual_angle = calcular_angulo(new_antenna_pole)/DEG2RAD
-    #     # Ver se ângulo maior é maior que o ângulo esperado
-    #     if actual_angle > max_angle_value_read:
-    #         #Salvar parâmetros
-    #         max_angle_found = True
-    #         max_angle_depth = actuator_len
-    #         max_angle_value_read = actual_angle
-    #     #Ver se ele é menor que o menor ângulo maior que o esperado
-    #     if actual_angle < min_angle_value_read:
-    #         min_angle_found = True
-    #         min_angle_depth = actuator_len
-    #         min_angle_value_read = actual_angle
-    #     for part in [pole,new_hinge_pos,new_act_pos,new_active_act_pos,new_antenna_pole]:
-    #         x_coords,y_coords = vector2matplot(part)
-    #         plt.plot(x_coords, y_coords, marker='o',color=colors[numcolor%len(colors)])
-    #     numcolor += 1
-    # plt.show()
-
-
+    pole = best_guess_params[0]
+    antenna_hinge = best_guess_params[1]
+    antenna_pole = pole[0]
+    result_array = np.add(pole[0], (0,0.1)) 
+    result_tuple = [pole[0],tuple(result_array)]
+    antenna_pole = result_tuple
+    numcolor = 21
+    for actuator_len in np.arange(calcular_tamanho(ref_actuator)*LOWBORDER,(calcular_tamanho(ref_actuator)*HIGHBORDER),calcular_tamanho(ref_actuator)/STEPS):
+        active_actuator = [(0,0),(actuator_len,0)]
+        try:
+            hinge_angle = calcular_triangulo(calcular_tamanho(active_actuator),calcular_tamanho(pole),calcular_tamanho(antenna_hinge))
+            actuator_angle = calcular_triangulo(calcular_tamanho(antenna_hinge),calcular_tamanho(pole),calcular_tamanho(active_actuator))
+        except:
+            continue
+        new_hinge_pos = calcular_posicao(antenna_hinge,hinge_angle+(270*DEG2RAD))
+        new_active_act_pos = calcular_posicao(active_actuator,(90*DEG2RAD)-actuator_angle)
+        new_act_pos = calcular_posicao(ref_actuator,(90*DEG2RAD)-actuator_angle)
+        new_antenna_pole = calcular_posicao(antenna_pole,(90*DEG2RAD)+hinge_angle-(best_guess_correction*DEG2RAD))
+        # Calcular sensibilidade do sistema
+        # Ângulo atual da antena
+        actual_angle = calcular_angulo(new_antenna_pole)/DEG2RAD
+        # Ver se ângulo maior é maior que o ângulo esperado
+        if actual_angle > max_angle_value_read:
+            #Salvar parâmetros
+            max_angle_found = True
+            max_angle_depth = actuator_len
+            max_angle_value_read = actual_angle
+        #Ver se ele é menor que o menor ângulo maior que o esperado
+        if actual_angle < min_angle_value_read:
+            min_angle_found = True
+            min_angle_depth = actuator_len
+            min_angle_value_read = actual_angle
+        for part in [pole,new_hinge_pos,new_act_pos,new_active_act_pos,new_antenna_pole]:
+            x_coords,y_coords = vector2matplot(part)
+            plt.plot(x_coords, y_coords, marker='o',color=colors[numcolor%len(colors)])
+        numcolor += 1
+    plt.show()
+    input('Wait to end...')
 
 if __name__ == "__main__":
     main()
